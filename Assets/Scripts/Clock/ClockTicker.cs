f6435e9d8697ab60043147528bf65a6f6f504177
Hugo Beaucamps <hugo.beaucamps@jooxter.com>
2022-04-02 15:46:25 +0200

    Setup global clock

---
+++ Assets/Scripts/Clock/ClockTicker.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlanchaCorp.LD50.ScriptableObjects;
+
+namespace PlanchaCorp.LD50.Scripts {
+    public class ClockTicker : MonoBehaviour
+    {
+
+        [SerializeField]
+        private FloatVariable tickDelay;
+        [SerializeField]
+        private GameEvent onTickEvent;
+        void OnEnable()
+        {
+            StartCoroutine("tick");
+        }
+        void OnDisable(){
+            StopCoroutine("tick");
+        }
+        IEnumerator tick(){
+            for(;;){
+                onTickEvent.Raise();
+                yield return new WaitForSeconds(tickDelay.Value);
+            }
+        }
+    }
+}
