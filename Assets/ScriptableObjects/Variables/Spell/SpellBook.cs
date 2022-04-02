ccc6f867ec6bfb72871dc959159d0d38a5cc0ca3
Hugo Beaucamps <hugo.beaucamps@jooxter.com>
2022-04-02 15:23:08 +0200

    spell lifecycle

---
+++ Assets/ScriptableObjects/Variables/Spell/SpellBook.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlanchaCorp.LD50.ScriptableObjects{
+    [CreateAssetMenu(menuName = "Scriptable Objects/Spell book")]
+    public class SpellBook : ScriptableObject
+    {
+        [SerializeField]
+        List<GameObject> spells;
+    }
+}
--- Assets/Scripts/Spell/SpellAction.cs
+++ Assets/Scripts/Spell/SpellAction.cs
@@ -1,26 +1,48 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 
 namespace PlanchaCorp.LD50.Scripts.Spell {
     public class SpellAction : MonoBehaviour
     {
+        private float endTime;
+        private float amount;
+        private List<GameObject> entities;
         // Start is called before the first frame update
         void Start()
         {
-            
+            entities = new List<GameObject>();
+            StartCoroutine("tick");
+        }
+        void OnDisable(){
+            StopAllCoroutines();
         }
 
         // Update is called once per frame
         void Update()
         {
-            
+            if(Time.time > endTime ){
+                Destroy(gameObject);
+            }
+        }
+
+        void cast(float duration,float amount,float scale){
+            endTime = Time.time + duration;
+            this.transform.localScale *= scale;
+            this.amount = amount;
         }
 
         public void OnTriggerEnter2D(Collider2D collider){
-
         }
         public void OnTriggerExit2D(Collider2D collider){
 
+        }
+        IEnumerator tick(){
+            while(true){
+            Debug.Log("heal for" + amount);
+            yield return new WaitForSeconds(amount);
+            }
         }
     }
 }
