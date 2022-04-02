6d51c549dd36fe1a760423259a12304c6ceced70
Hugo Beaucamps <hugo.beaucamps@jooxter.com>
2022-04-02 14:09:20 +0200

    setup prefab

---
+++ Assets/Scripts/Spell/SpellAction.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+
+namespace PlanchaCorp.LD50.Scripts.Spell {
+    public class SpellAction : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        public void OnTriggerEnter2D(Collider2D collider){
+
+        }
+        public void OnTriggerExit2D(Collider2D collider){
+
+        }
+    }
+}
