8b06b308c29456b853892d7e4275922420681171
Hugo Beaucamps <hugo.beaucamps@jooxter.com>
2022-04-04 01:26:35 +0200

    Upgradable skill and Cooldown on splash

--- Assets/SkillUIManager.cs
+++ Assets/SkillUIManager.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PlanchaCorp.LD50.ScriptableObjects;
+using UnityEngine.UI;
 
 public class SkillUIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField]
+    private GameObject upgradeButton;
+    [SerializeField]
+    private Image cooldownMask;
+    [SerializeField]
+    private TMPro.TMP_Text timer;
+
+    private Button spellButton;
+
+    void Start() {
+        spellButton = GetComponent<Button>();
+    }
+    void Update(){
+    }
+    public void setClickable() {
+        spellButton.interactable = true;
+    }
+    public void coolDown(float delay) {
+        spellButton.interactable = false;
+        StartCoroutine("coolDownProgress",delay);
+    }
+    public void OnSpellCasted(GameEvent gameEvent){
+        AbstractSpellType spellType = gameEvent.Get<AbstractSpellType>();
+        this.coolDown(spellType.Cooldown);
+    }
+    public void setNotClickable() {
+        spellButton.interactable = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void DisplayUpgradeButton() {
+        upgradeButton.SetActive(true);
+    }
+    public void HideUpgradeButton() {
+        upgradeButton.SetActive(false);
+    } 
+    IEnumerator coolDownProgress(float delay){
+        for(float time = 0;time <= delay;time += .1f) {
+            cooldownMask.fillAmount = 1 - (time/delay);
+            timer.text = "" + Mathf.Ceil(delay - time);
+            yield return new WaitForSeconds(.1f);
+        }
+        timer.text = "";
+        spellButton.interactable = true;
+
     }
 }
