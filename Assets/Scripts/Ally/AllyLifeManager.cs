98f0792e6045f59fe401e00a38a05b1520b3b457
Zessirb <zessirb@gmail.com>
2022-04-04 16:47:24 +0200

    Adding footsteps sound

--- Assets/Scripts/Ally/AllyLifeManager.cs
+++ Assets/Scripts/Ally/AllyLifeManager.cs
@@ -5,21 +5,33 @@ using PlanchaCorp.LD50.ScriptableObjects;
 
 namespace PlanchaCorp.LD50.Scripts
 {
+    [RequireComponent(typeof(SpriteRenderer))]
     public class AllyLifeManager : MonoBehaviour
     {
+        [SerializeField]
+        private FloatVariable maxHealth;
+        
         [SerializeField]
         private GameEventPublisher allyDeath;
         [SerializeField]
         private GameEventPublisher expEmit;
         [SerializeField]
-        private FloatVariable maxHealth;
+        private AudioSource[] deathSounds;
+        [SerializeField]
+        private SpriteRenderer spriteRenderer;
+
         public float currentHealth;
         private HealthBar healthBar;
+
+        private float deathTime;
+        private bool isDying;
 
         void Start()
         {
             this.currentHealth = this.maxHealth.Value;
             this.healthBar = GetComponentInChildren<HealthBar>();
+            isDying = false;
+            deathTime = 0;
         }
 
         void Update()
@@ -27,6 +39,9 @@ namespace PlanchaCorp.LD50.Scripts
             this.healthBar.UpdateSlider(currentHealth,maxHealth.Value);
             if(this.currentHealth<=0){
                 Die();
+            }
+            if (isDying) {
+                deathTime += Time.deltaTime;
             }
         }
         public void Hurt(GameEvent gameEvent) {
@@ -43,9 +58,21 @@ namespace PlanchaCorp.LD50.Scripts
             this.currentHealth = newHealth;
             expEmit.Raise(new GameEvent(xpGain));
         }
+
         public void Die(){
             GameEvent deathEvent = new GameEvent(this);
             allyDeath.Raise(deathEvent);
+            StartCoroutine(Die(2));
+        }
+
+        public IEnumerator Die(float delay) {
+            float opacity = Mathf.Lerp(delay, 0, deathTime);
+            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, opacity);
+            if (deathSounds.Length > 0) {
+                int randomSoundId = Mathf.FloorToInt(Random.Range(0, deathSounds.Length - 0.01f));
+                deathSounds[randomSoundId].Play();
+            }
+            yield return new WaitForSeconds(delay);
             Destroy(gameObject);
         }
     }
